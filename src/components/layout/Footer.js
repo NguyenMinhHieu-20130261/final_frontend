9ba5717adcbfa2189e3ce30b3ebc635e057bd989
NguyenMinhHieu <20130261@st.hcmuaf.edu.vn>
2023-06-09 13:29:11 +0700

    chỉnh lại header, footer

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,20 +1,81 @@
 import React from 'react';
-import '../../assets/css/layout/footer.css'
-import '../../assets/css/main.css'
 const Footer = () => {
     return (
         <div id="footer">
-            <div className="left">&copy; 2003-2009 Simple Magazine <span className="text-separator">&rarr;</span> <a
-                href="#">Home</a>
-                <span className="text-separator">|</span><a href="#">News</a> <span className="text-separator">|</span>
-                <a
-                    href="#">Politics</a> <span className="text-separator">|</span> <a href="#">Culture</a> <span
-                    className="text-separator">|</span> <a href="#">Sport</a> <span className="text-separator">|</span>
-                <a
-                    href="#">Debate</a> <span className="text-separator">|</span> <a href="#">Entertainment</a></div>
-            <div className="right"><a href="http://templates.arcsin.se/">Website template</a> by <a
-                href="http://arcsin.se/">Arcsin</a></div>
-            <div className="clearer">&nbsp;</div>
+            <div id="ft-top">
+                <div className="ft-top-container first-ft">
+                    <ul className="ft-top-text">
+                        <li><a href=""> Trang Chủ</a></li>
+                        <li><a href=""> Trang Chủ</a></li>
+                        <li><a href=""> Trang Chủ</a></li>
+                        <li><a href=""> Trang Chủ</a></li>
+                    </ul>
+                </div>
+                <div className="ft-top-container">
+                    <ul className="ft-top-text">
+                        <li><a href=""> Trang Chủ</a></li>
+                        <li><a href=""> Trang Chủ</a></li>
+                        <li><a href=""> Trang Chủ</a></li>
+                        <li><a href=""> Trang Chủ</a></li>
+                    </ul>
+                </div>
+                <div className="ft-top-container">
+                    <ul className="ft-top-text">
+                        <li><a href=""> Trang Chủ</a></li>
+                        <li><a href=""> Trang Chủ</a></li>
+                        <li><a href=""> Trang Chủ</a></li>
+                        <li><a href=""> Trang Chủ</a></li>
+                    </ul>
+                </div>
+                <div className="ft-top-container">
+                    <ul className="ft-top-text">
+                        <li><a href=""> Trang Chủ</a></li>
+                        <li><a href=""> Trang Chủ</a></li>
+                        <li><a href=""> Trang Chủ</a></li>
+                        <li><a href=""> Trang Chủ</a></li>
+                    </ul>
+                </div>
+                <div className="ft-top-container">
+                    <ul className="ft-top-text">
+                        <li><a href=""> Trang Chủ</a></li>
+                        <li><a href=""> Trang Chủ</a></li>
+                        <li><a href=""> Trang Chủ</a></li>
+                        <li><a href=""> Trang Chủ</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div id="ft-bottom">
+                <div id="ft-bot-content">
+                    <div id="ft-bot-left">
+                        <table className="ft-bot-table">
+                            <tr>
+                                <th>Hotline</th>
+                                <td>0909090909</td>
+                            </tr>
+                            <tr>
+                                <th>Địa chỉ</th>
+                                <td>1234 đường 1234 khu phố 123 phường một hai ba tp.123 </td>
+                            </tr>
+                            <tr>
+                                <th>tiêu đề</th>
+                                <td>dữ liệu</td>
+                            </tr>
+                            <tr>
+                                <th>tiêu đề</th>
+                                <td>dữ liệu</td>
+                            </tr>
+                        </table>
+                    </div>
+                    <div id="ft-bot-right">
+                        <div className="ft-txt-cnt">
+                            <div className="ft-txt"><a></a></div>
+                            <div className="ft-txt"><a></a></div>
+                            <div className="ft-txt"><a></a></div>
+                            <div className="ft-txt"><a></a></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
