c5661cac376410e531620f6e1e9c6a1dae14d17e
NguyenMinhHieu <20130261@st.hcmuaf.edu.vn>
2023-06-10 18:00:32 +0700

    làm lại header footer

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 
 import Header from "./components/layout/Header";
 import Footer from "./components/layout/Footer";
-import Network from "./components/layout/Network";
-import '../src/assets/css/main.css'
 import {Home} from "./components/MainPage";
 
 function App() {
   return (
       <div className="App">
-          <Network/>
         <div id="site">
             <div className="center-wrapper">
                 <Header/>
